3490546f8364adeae9c0ed702503f3ea24095fa4
DarkKing056 <Barakatd190254@sus.htl-grieskirchen.at>
2022-09-26 12:11:28 +0200

    commit 5:Aus datei informationen rauslesen

--- hue2POS/src/NumberTester.java
+++ hue2POS/src/NumberTester.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NumberTester {
     private NumberTest oddTester;
     private NumberTest primeTester;
     private Object palindromeTester;
+
+    private String fileName;
 
     public NumberTester(String fileName){
 
@@ -18,6 +24,25 @@ public class NumberTester {
         this.palindromeTester=palindromeTester;
     }
     public void testFile(){
+        try {
+            Scanner scanner=new Scanner(new File(fileName));
+            while(scanner.hasNextLine()){
+                String[] zeile=scanner.nextLine().split(" ");
+                int[] numbers=new int[zeile.length];
 
+                for (int i = 0; i < zeile.length; i++) {
+                    numbers[i]=Integer.parseInt(zeile[i]);
+                }
+                if (numbers.length == 2) {
+                    switch (numbers[0]){
+                        case 1:break;
+                        case 2:break;
+                        case 3:break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
