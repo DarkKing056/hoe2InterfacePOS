abcaf685e0aec5c65bfeb9bbc889482a68da741a
DarkKing056 <Barakatd190254@sus.htl-grieskirchen.at>
2022-10-02 19:19:35 +0200

    RationalCalculator is done

--- hue2POS/src/RationalCalculator.java
+++ hue2POS/src/RationalCalculator.java
@@ -1,2 +1,31 @@
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator{
+    public RationalCalculator() {
+        super((a, b) -> {
+            return new Number(a.getA()*b.getB()+(b.getA()*a.getB()),a.getB()*b.getB());
+        }, (a,b)->{
+            return new Number(a.getA()*b.getB()-(b.getA()*a.getB()),a.getB()*b.getB());
+        }, (a,b)->{return new Number(a.getA()*b.getA(),a.getB()*b.getB());},
+                (a,b)->
+                    new Number(a.getA() * b.getB(), a.getB() * a.getA()));
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return add.calc(a,b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return subtract.calc(a,b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divide.calc(a,b);
+    }
 }
